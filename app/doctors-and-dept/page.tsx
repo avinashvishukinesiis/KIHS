773aca4c32e834075515bba7a198ceebd52238f5
avinash@kinesiis.in <avinash@kinesiis.in>
2025-04-10 14:00:46 +0530

    removed usestate from doctor and dept

--- app/doctors-and-dept/page.tsx
+++ app/doctors-and-dept/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { useState } from "react";
 import { PvSection1 } from "@/Components/PatientsAndVisitiors/PvSection1/PvSection1";
 import { DsSection2 } from "@/Components/Doctors&Dept/DsSection2/DsSection2";
 
